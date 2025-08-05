996c0f1705d09646ec2bd4db06de0875d5120042
Lorin3766 <antonseregin3766@gmail.com>
2025-08-04 20:16:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -181,3 +181,4 @@ if __name__ == "__main__":
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.add_handler(CallbackQueryHandler(feedback_callback))
     app.run_polling()
+
